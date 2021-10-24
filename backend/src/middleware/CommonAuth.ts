c0dbb0e743d32ae2d65839cddc6fe90fbdba8937
kishibe-rohan <devrohan2000@gmail.com>
2021-10-24 17:09:40 +0530

    completed password and notification utils

---
+++ backend/src/middleware/CommonAuth.ts
@@ -0,0 +1,20 @@
+import {Request,Response,NextFunction} from 'express'
+import { AuthPayload } from '../dto'
+import { ValidateSignature } from '../utils'
+
+declare global{
+    namespace Express{
+        interface Request{
+            user?:AuthPayload
+        }
+    }
+}
+
+export const Authenticate = async(req:Request,res:Response,next:NextFunction) => {
+    const signature = await ValidateSignature(req);
+    if(signature)
+    return next();
+    else{
+        return res.json({message:"Authorization Error"})
+    }
+}
---
+++ backend/src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './PasswordUtil';
+export * from './NotificationUtil';

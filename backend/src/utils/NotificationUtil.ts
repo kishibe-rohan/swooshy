00fd2ce3ad6e25f082c34c215535b8182d509be6
kishibe-rohan <devrohan2000@gmail.com>
2021-10-24 13:56:59 +0530

    added DTO for customer,vendor,food

---
+++ backend/src/utils/NotificationUtil.ts
@@ -0,0 +1,21 @@
+export const GenerateOtp = () => {
+    const otp = Math.floor(10000 + Math.random() * 900000);
+    let expiry = new Date()
+    expiry.setTime(new Date().getTime() +  (30 * 60 * 1000))
+
+    return {otp,expiry};
+}
+
+export const onRequestOtp = async(otp:number,toPhoneNumber:string) => {
+    const accountSid = "ACCOUNT_ID";
+    const authToken = "AUTH_TOKEN";
+    const client = require('twilio')(accountSid,authToken)
+
+    const response = await client.message.create({
+        body: `Your OTP is ${otp}`,
+        from: 'PHONE_NUMBER',
+        to:`${toPhoneNumber}`
+    })
+
+    return response;
+}
